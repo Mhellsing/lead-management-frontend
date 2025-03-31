d0284397999661550fb213620eb54cef821c0917
Mhellsing <marcusmalkav@gmail.com>
2025-03-31 13:46:40 -0300

    criando leadCard

---
+++ src/utils/DateUtils.js
@@ -0,0 +1,11 @@
+export const formatDate = (dateString) => {
+    const date = new Date(dateString);
+        
+    const monthDayFormatter = new Intl.DateTimeFormat('en-US', { month: 'long', day: 'numeric' });
+    const formattedMonthDay = monthDayFormatter.format(date);
+        
+    const timeFormatter = new Intl.DateTimeFormat('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+    const formattedTime = timeFormatter.format(date);
+    
+    return `${formattedMonthDay} @ ${formattedTime}`;
+   };
